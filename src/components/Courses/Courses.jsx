2bf1a7758afe4cfd8eeebf8f449d29dca5f24c6b
Diana064 <dipustolyakova02@gmail.com>
2023-03-16 18:03:53 +0200

    add

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
 import * as ImageService from '../services/api';
 import { useLocation } from 'react-router-dom';
 import Pagination from 'components/Pagination/Pagination';
--- src/components/Courses/CoursesItem/CoursesItem.jsx
+++ src/components/Courses/CoursesItem/CoursesItem.jsx
@@ -1,36 +1,12 @@
-import ReactPlayer from 'react-player';
 import { HoverVideo } from 'components/HoverVideo/HoverVideo';
-import HoverVideoPlayer from 'react-hover-video-player';
+
 import { nanoid } from 'nanoid';
 export const CoursesItem = ({ course }) => {
   const img = `${course.previewImageLink}/cover.webp`;
   return (
     <>
       <h2>{course.title}</h2>
-      {/* <HoverVideoPlayer
-        videoSrc={{
-          type: 'video/hls',
-          src: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
-        }}
-        pausedOverlay={
-          <img
-            src={img}
-            alt=""
-            style={{
-              // Make the image expand to cover the video's dimensions
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-            }}
-          />
-        }
-        loadingOverlay={
-          <div className="loading-overlay">
-            <div className="loading-spinner" />
-          </div>
-        }
-      /> */}
-      {/* <ReactPlayer url={course.meta.courseVideoPreview.link} type="video/hls" /> */}
+
       <HoverVideo
         src="https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
         img={img}
